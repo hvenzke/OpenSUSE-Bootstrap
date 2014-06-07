971bf065c9840f768a56d86aed310e4eaf2172db
Remsnet LTD, <remsnet@users.noreply.github.com>
2014-06-07 03:37:14 +0200

    Create README.mk-mini-root.sh

---
+++ README.mk-mini-root.sh
@@ -0,0 +1,6 @@
+README mk-mini-root.sh
+
+The script mk-mini-root.sh creates an mini chroot.
+
+This was created to verify  rinse´s 2.0.1 ´s  incorrect  unpackPackages{} behavior on opensuse 12.x and up.
+Automated OpenSuse  CORE  require sutch at once bevor anything else starts up - when used _without_ yast.
